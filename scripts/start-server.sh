82af8213588011885cdf50aa4d4ee7476e487e8c
Juhani Kumara <juhani.kumara@gmail.com>
2023-07-19 22:00:29 +0300

    simplify dev server startup

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 DIR_PATH=$(dirname "$(realpath "${BASH_SOURCE:-$0}")")
-npx http-server "$DIR_PATH/.." -S -C "$LOCAL_CERT" -K "$LOCAL_KEY"  -c-1 -p "$PORT"
+npx http-server "$DIR_PATH/.." -c-1 -p "$PORT"
